c5368272247be8e7440457c90e71c07b70f9e8cc
somrat sorkar <themefisher.dev@gmail.com>
2023-03-30 14:30:29 +0600

    update tab and accordion

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -26,22 +26,60 @@
   });
 
   // for tab component
-  const tabs = document.querySelectorAll(".tabs>li");
-  const tabElements = document.querySelectorAll(".tab-element");
+  // Get all the tab groups on the page
+  const tabGroups = document.querySelectorAll("[data-tab-group]");
+  // Loop through each tab group
+  tabGroups.forEach((tabGroup) => {
+    // Get the tabs nav and content for this tab group
+    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+    const tabsNavItem = tabsNav.querySelectorAll("[data-tab]");
 
-  tabs.forEach((tab, index) => {
-    tab.addEventListener("click", () => {
-      // remove active class
-      tabs.forEach((tab) => tab.classList.remove("active-tab"));
-      tabElements.forEach((tabElement) =>
-        tabElement.classList.remove("active-tab")
-      );
+    // Get the active tab index from local storage, or default to 0 if not set
+    const activeTabName =
+      localStorage.getItem(`activeTabName-${tabGroup.dataset.tabGroup}`) ||
+      tabsNavItem[0].getAttribute("data-tab");
 
-      // add active clas
-      tab.classList.add("active-tab");
-      tabElements[index].classList.add("active-tab");
+    // Set the active tab
+    setActiveTab(tabGroup, activeTabName);
+
+    // Add a click event listener to each tab nav item
+    tabsNavItem.forEach((tabNavItem) => {
+      tabNavItem.addEventListener("click", () => {
+        // Get the index of the clicked tab nav item
+        const tabName = tabNavItem.dataset.tab;
+        setActiveTab(tabGroup, tabName);
+
+        // Save the active tab index to local storage
+        localStorage.setItem(
+          `activeTabName-${tabGroup.dataset.tabGroup}`,
+          tabName
+        );
+      });
     });
   });
+
+  // Function to set the active tab for a given tab group
+  function setActiveTab(tabGroup, tabName) {
+    // Get the tabs nav and content for this tab group
+    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+    const tabsContent = tabGroup.querySelector("[data-tab-content]");
+
+    // Remove the active class from all tab nav items and content panes
+    tabsNav.querySelectorAll("[data-tab]").forEach((tabNavItem) => {
+      tabNavItem.classList.remove("active");
+    });
+    tabsContent.querySelectorAll("[data-tab-panel]").forEach((tabPane) => {
+      tabPane.classList.remove("active");
+    });
+
+    // Add the active class to the selected tab nav item and content pane
+    const selectedTabNavItem = tabsNav.querySelector(`[data-tab="${tabName}"]`);
+    selectedTabNavItem.classList.add("active");
+    const selectedTabPane = tabsContent.querySelector(
+      `[data-tab-panel="${tabName}"]`
+    );
+    selectedTabPane.classList.add("active");
+  }
 
   // modal components
   const modal = document.getElementById("modal");
@@ -68,20 +106,11 @@
   });
 
   // Accordion component
-  const accordionButtons = document.querySelectorAll(".accordion-button");
-  accordionButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const content = button.nextElementSibling;
-      if (content.classList.contains("hidden")) {
-        console.log(button.children[0]);
-        content.classList.remove("hidden", "h-0");
-        content.classList.add("opacity-100", "h-auto");
-        button.children[0].classList.add("rotate-180");
-      } else {
-        content.classList.remove("opacity-100", "h-auto");
-        content.classList.add("opacity-0", "h-0", "overflow-hidden", "hidden");
-        button.children[0].classList.remove("rotate-180");
-      }
+  const ghtmAccordion = document.querySelectorAll("[data-accordion]");
+  ghtmAccordion.forEach((header) => {
+    header.addEventListener("click", () => {
+      const accordionItem = header.parentElement;
+      accordionItem.classList.toggle("active");
     });
   });
 
